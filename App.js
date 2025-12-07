6f90a42b1584c4f08ab0c2da02b6c99ef020f235
Hi776222 <152708143+Hi776222@users.noreply.github.com>
2025-12-07 16:31:36 +0100

    Premier commit: ajout du projet MonApp

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+import AppBar from './components/AppBar';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Accueil" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <SafeAreaProvider>
+     
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#007AFF' }}>
+        
+        <NavigationContainer>
+          
+          <AppBar />
+
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              headerShown: false, 
+              tabBarActiveTintColor: 'blue',
+              tabBarInactiveTintColor: 'gray',
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'Maison') {
+                  iconName = focused ? 'home' : 'home-outline';
+                } else if (route.name === 'Paramètres') {
+                  iconName = focused ? 'settings' : 'settings-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+          >
+            <Tab.Screen name="Maison" component={HomeStack} />
+            <Tab.Screen name="Paramètres" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
